d1f904ffe54b90f762454600ab22781739ad6386
Marcela Torres <itsmarcy@gmail.com>
2023-10-31 16:45:32 -0500

    Add .vscode to gitignore

--- src/__main__.py
+++ src/__main__.py
@@ -1,14 +1,29 @@
 """
-This file allows running the parent directory directly:
-python3 src [ arg1 ... ]
-
-This also allows packaging up as a zipapp.
-
-If you do not need this functionality:
-  - Remove this file.
-  - Consider moving minimalhello out of src.
+By naming the top-level module __main__, it is possible to run this module as
+a script by running:
+`python -m minimalhello [args...]`
 """
 
-import minimalhello.__main__
+from sys import argv, path
+from textwrap import dedent
 
-minimalhello.__main__.main()
+import __version__
+
+
+def main():
+    print(
+        dedent(
+            f"""\
+            {__package__=} (v{__version__})
+            {__name__=} @ {__file__}
+
+            {argv=}
+            """  # This f-string syntax requires 3.8+
+        )
+    )
+    for p in path:
+        print(p)
+
+
+if __name__ == "__main__":
+    main()

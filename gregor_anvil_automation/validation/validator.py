b928baf1cd9e452295e29c296cce997ec59ab1ac
Ryan Tunkel <123666075+ryanetunkel@users.noreply.github.com>
2023-11-17 10:03:10 -0600

    fixed bugs, added num_or_na

--- gregor_anvil_automation/validation/validator.py
+++ gregor_anvil_automation/validation/validator.py
@@ -7,20 +7,24 @@ class SampleValidator(Validator):
 
     def _check_with_is_number(self, field: str, value: str):
         """Checks that the field's value is a valid integer"""
-        if isinstance(value, int):
+        if not isinstance(value, int):
             self._error(field, "Value requires an int")
+
+    def _check_with_is_number_or_na(self, field: str, value: str):
+        if not value != "NA" and not isinstance(value, int):
+            self._error(field, "Value must be NA or an int")
 
     def _check_with_must_start_with_bcm_fam(self, field: str, value: str):
         """Checks that field's value starts with `BCM_Fam`"""
         if not value.startswith("BCM_Fam"):
             self._error(field, "Value must start with BCM_Fam")
 
-    def _check_with_must_start_with_bcm_subject(self, field: str, value: str):
+    def _check_with_must_start_with_bcm_subject_bh(self, field: str, value: str):
         """Checks that field's value starts with `BCM_Subject`"""
-        if not value.startswith("BCM_Subject"):
-            self._error(field, "Value must start with BCM_Subject")
+        if not value.startswith("BCM_Subject_BH"):
+            self._error(field, "Value must start with BCM_Subject_BH")
 
-    def _normalize_coerce_uppercase(self, value: str) -> str:
+    def _normalize_coerce_camelcase(self, value: str) -> str:
         """Coerces value to camelcase"""
         value = value[0].upper() + value[1:].lower()
         return value

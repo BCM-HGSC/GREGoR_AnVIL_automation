f4803dd546164fedc584b7fabd008e30c2f457fd
Ryan Tunkel <123666075+ryanetunkel@users.noreply.github.com>
2023-11-17 14:00:59 -0600

    uniqueness, exist_in_source error added

--- gregor_anvil_automation/validation/checks.py
+++ gregor_anvil_automation/validation/checks.py
@@ -4,23 +4,19 @@ from ..utils.types import Table
 
 def check_uniqueness(samples: list[str]):
     """Checks if the given list of values is unique"""
-    error = []
-    # TODO: Add code
     for sample in samples:
-        other_id = sample[f"{id_name}"]
-        if other_id in subject_ids:
-            error_message = f"ID is already present in {samples}"
-            print(error_message)
-            errors.append(error_message)
-    return False
+        if samples.count(sample) > 1:
+            return False
+    return True
 
 
 def check_value_exist_in_source(field_name: str, table: Table, table_source: Table):
     """Checks that the values given exist in the source table.
     Example if participant table has family_id BCM_Fam_1234, this checks
     that BCM_Fam_1234 exist in the family table under `family_id`"""
+    error = f"Value does not exist in the table {field_name}"
     field_values = {sample[field_name] for sample in table_source}
-    invalid = {}
+    invalid = set()
     for sample in table:
         value = sample[field_name]
         if value not in field_values:

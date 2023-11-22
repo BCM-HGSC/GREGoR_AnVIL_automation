90066eb5d6f2069ccb4f01a8973d336dbaff61bf
Ryan Tunkel <123666075+ryanetunkel@users.noreply.github.com>
2023-11-22 10:15:16 -0600

    relatives id checks, no participant_id comparison

--- gregor_anvil_automation/validation/sample.py
+++ gregor_anvil_automation/validation/sample.py
@@ -32,6 +32,13 @@ class SampleValidator(Validator):
             - "0" must be accepted as valid input
         """
         # TODO: Please fill out.
+        if value != 0 and (
+            not value.startswith("BCM_Subject_") or not value.endswith("_2")
+        ):  # add and not the subject id match, WIP
+            self._error(
+                field,
+                "Value must be 0 or match the format of BCM_Subject_######_2, and match the subject id in participant_id",
+            )
 
     def _check_with_paternal_id_is_valid(self, field: str, value: str):
         """Checks that paternal id is valid.
@@ -43,6 +50,13 @@ class SampleValidator(Validator):
             - "0" must be accepted as valid input
         """
         # TODO: Please fill out.
+        if value != 0 and (
+            not value.startswith("BCM_Subject_") or not value.endswith("_3")
+        ):  # add and not the subject id match, WIP
+            self._error(
+                field,
+                "Value must be 0 or match the format of BCM_Subject_######_3, and match the subject id in participant_id",
+            )
 
     def _check_with_twin_id_is_valid(self, field: str, value: str):
         """Checks that twin id is valid.
@@ -56,6 +70,32 @@ class SampleValidator(Validator):
         """
         # TODO: Please fill out.
         # NOTE: Twin ID field looks like this for reference: BCM_Subject_BH10325_1 BCM_Subject_BH10325_4
+        error = [
+            "Value must be NA or:",
+            "Value must contain two strings separated by a space.",
+            "One string must match the format of BCM_Subject_######_1.",
+            "The other string must match the format of BCM_Subject_######_4.",
+            "Both strings must have the same subject_id.",
+            "One of the strings must be the same as participant_id.",
+        ]
+        if value != "NA":
+            if value.count(" ") == 1:
+                id1 = value.split(" ")[0]
+                id2 = value.split(" ")[1]
+                start_idx = len("BCM_Subject_")
+                end_idx1 = len(id1) - len("_#")
+                end_idx2 = len(id2) - len("_#")
+                if (
+                    not id1.startswith("BCM_Subject_")
+                    or not id2.startswith("BCM_Subject_")
+                    or not (
+                        (id1.endswith("_4") and id2.endswith("_1"))
+                        or (id1.endswith("_1") and id2.endswith("_4"))
+                    )
+                    or id1[start_idx:end_idx1] != id2[start_idx:end_idx2]
+                ):
+                    # Need to add check for being same as participant_id
+                    self._error(field, "\n".join(error))
 
     def _check_with_must_start_with_bcm(self, field: str, value: str):
         """Checks that field's value starts with `BCM_`"""

8c34519d952109db4ba1c2d47ec41ce419a2b981
Marcela Torres <itsmarcy@gmail.com>
2023-11-29 12:59:48 -0600

    Revert back to using addict

--- gregor_anvil_automation/utils/utils.py
+++ gregor_anvil_automation/utils/utils.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import addict
 import yaml
 from openpyxl import Workbook, load_workbook
 
@@ -39,7 +40,7 @@ def get_table_samples(input_file: Path) -> dict[str, list[Sample]]:
     return table_samples
 
 
-def parse_yaml(yaml_path: Path) -> dict:
+def parse_yaml(yaml_path: Path) -> addict.Dict:
     """Parses a yaml file and return Iterator"""
     with open(yaml_path, encoding="utf-8") as fin:
-        return dict == (yaml.safe_load(fin.read()))
+        return addict.Dict(yaml.safe_load(fin.read()))

78928d75ca61efa94a4fa5cb46d24a1b2ba07104
Marcela Torres <itsmarcy@gmail.com>
2024-01-05 11:02:23 -0600

    rename test file

--- tests/validation/test_checks.py
+++ tests/validation/test_checks.py
@@ -1,3 +1,4 @@
+import addict
 import pytest
 
 from gregor_anvil_automation.validation.checks import check_uniqueness
@@ -5,38 +6,42 @@ from gregor_anvil_automation.validation.checks import check_uniqueness
 
 @pytest.fixture(name="uniqueness_sample", scope="function")
 def fixture_uniqueness_sample():
-    return {
-        "aligned_dna_short_read_id": "BCM_BHTEST_test-batch_id",
-        "experiment_dna_short_read_id": "BCM_BHTEST",
-        "aligned_dna_short_read_file": "",
-        "aligned_dna_short_read_index_file": "",
-        "md5sum": "test-uniqueness-gregor",
-        "reference_assembly": "GRCh38",
-        "reference_assembly_uri": "NA",
-        "reference_assembly_details": "test-uniqueness-gregor",
-        "alignment_software": "test-uniqueness-gregor",
-        "mean_coverage": "test-uniqueness-gregor",
-        "analysis_details": "test-uniqueness-gregor",
-        "quality_issues": "test-uniqueness-gregor",
-    }
+    return addict.Dict(
+        {
+            "aligned_dna_short_read_id": "BCM_BHTEST_test-batch_id",
+            "experiment_dna_short_read_id": "BCM_BHTEST",
+            "aligned_dna_short_read_file": "",
+            "aligned_dna_short_read_index_file": "",
+            "md5sum": "test-uniqueness-gregor",
+            "reference_assembly": "GRCh38",
+            "reference_assembly_uri": "NA",
+            "reference_assembly_details": "test-uniqueness-gregor",
+            "alignment_software": "test-uniqueness-gregor",
+            "mean_coverage": "test-uniqueness-gregor",
+            "analysis_details": "test-uniqueness-gregor",
+            "quality_issues": "test-uniqueness-gregor",
+        }
+    )
 
 
 @pytest.fixture(name="uniqueness_sample2", scope="function")
 def fixture_uniqueness_sample2():
-    return {
-        "aligned_dna_short_read_id": "BCM_BHTEST2_test-batch_id",
-        "experiment_dna_short_read_id": "BCM_BHTEST2",
-        "aligned_dna_short_read_file": "",
-        "aligned_dna_short_read_index_file": "",
-        "md5sum": "test-uniqueness-gregor2",
-        "reference_assembly": "GRCh38",
-        "reference_assembly_uri": "NA",
-        "reference_assembly_details": "test-uniqueness-gregor2",
-        "alignment_software": "test-uniqueness-gregor2",
-        "mean_coverage": "test-uniqueness-gregor2",
-        "analysis_details": "test-uniqueness-gregor2",
-        "quality_issues": "test-uniqueness-gregor2",
-    }
+    return addict.Dict(
+        {
+            "aligned_dna_short_read_id": "BCM_BHTEST2_test-batch_id",
+            "experiment_dna_short_read_id": "BCM_BHTEST2",
+            "aligned_dna_short_read_file": "",
+            "aligned_dna_short_read_index_file": "",
+            "md5sum": "test-uniqueness-gregor2",
+            "reference_assembly": "GRCh38",
+            "reference_assembly_uri": "NA",
+            "reference_assembly_details": "test-uniqueness-gregor2",
+            "alignment_software": "test-uniqueness-gregor2",
+            "mean_coverage": "test-uniqueness-gregor2",
+            "analysis_details": "test-uniqueness-gregor2",
+            "quality_issues": "test-uniqueness-gregor2",
+        }
+    )
 
 
 @pytest.fixture(name="aligned_dna_short_read", scope="function")
@@ -64,14 +69,14 @@ def test_check_uniqueness_no_issues(aligned_dna_short_read):
     assert issues == []
 
 
-def test_check_uniqueness_sample_issues_no_initial_issues(AlignedShortRead):
+def test_check_uniqueness_sample_issues_no_initial_issues(uniqueness_sample):
     """Test that check_uniqueness sample passes validation with sample issues"""
-    table_name = "AlignedShortRead"
-    table = AlignedShortRead
+    table_name = "aligned_dna_short_read"
+    samples = [uniqueness_sample, uniqueness_sample]
     value = "aligned_dna_short_read_id"
     row = 1
     issues = []
-    issues = check_uniqueness(table, table_name, issues)
+    issues = check_uniqueness(samples, table_name, issues)
     assert issues == set(
         {
             "aligned_dna_short_read_id",

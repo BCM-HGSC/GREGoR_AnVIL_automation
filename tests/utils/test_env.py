e70c512796036d51e1191fc10c06f9866865aa72
Ryan Tunkel <123666075+ryanetunkel@users.noreply.github.com>
2024-01-24 16:47:10 -0600

    removed tmp_path, added pytest.raises

--- tests/utils/test_env.py
+++ tests/utils/test_env.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 
 from gregor_anvil_automation.utils.env import load_env_vars, find_and_load
@@ -16,40 +17,20 @@ def fixture_valid_env_file(tmp_path, common_file_path):
 
 
 def test_find_and_load_invalid(invalid_env_file):
-    try:
+    with pytest.raises(OSError):
         find_and_load(invalid_env_file)
-    except OSError:
-        assert True
 
 
-def test_find_and_load_valid(valid_env_file, monkeypatch):
-    monkeypatch.setenv("EMAIL_USERNAME", "hgsc@bcm.edu")
-    monkeypatch.setenv("EMAIL_PASSWORD", "123donthackme")
-    monkeypatch.setenv("EMAIL_HOST", "smtp.bcm.edu")
-    monkeypatch.setenv("EMAIL_USERNAME", "25")
-    monkeypatch.setenv("GOOGLE_APPLICATION_CREDENTIALS", "test-utils")
-    try:
-        find_and_load(valid_env_file)
-        assert True
-    except OSError:
-        assert False
+def test_find_and_load_valid(valid_env_file):
+    find_and_load(valid_env_file)
+    assert os.environ["ENVIRONMENT"] == "DEV"
 
 
 def test_load_env_vars_invalid(invalid_env_file):
-    try:
+    with pytest.raises(OSError):
         load_env_vars(invalid_env_file)
-    except OSError:
-        assert True
 
 
-def test_load_env_vars_valid(valid_env_file, monkeypatch):
-    monkeypatch.setenv("EMAIL_USERNAME", "hgsc@bcm.edu")
-    monkeypatch.setenv("EMAIL_PASSWORD", "123donthackme")
-    monkeypatch.setenv("EMAIL_HOST", "smtp.bcm.edu")
-    monkeypatch.setenv("EMAIL_USERNAME", "25")
-    monkeypatch.setenv("GOOGLE_APPLICATION_CREDENTIALS", "test-utils")
-    try:
-        load_env_vars(valid_env_file)
-        assert True
-    except OSError:
-        assert False
+def test_load_env_vars_valid(valid_env_file):
+    load_env_vars(valid_env_file)
+    assert os.environ["ENVIRONMENT"] == "DEV"

10b8a90f30f456f0e8bcd52b303bc567130d2396
Ryan Tunkel <123666075+ryanetunkel@users.noreply.github.com>
2024-01-26 13:16:16 -0600

    fixed test conflict with other files

--- tests/utils/test_env.py
+++ tests/utils/test_env.py
@@ -2,14 +2,10 @@ import os
 import pytest
 
 from gregor_anvil_automation.utils.env import (
-    find_dotenv,
-    load_dotenv,
     load_env_vars,
     find_and_load,
     ENVFileDoesnotExist,
 )
-
-import pprint
 
 
 @pytest.fixture(name="valid_env_file")
@@ -20,50 +16,27 @@ def fixture_valid_env_file():
 
 def test_find_and_load_invalid(tmp_path):
     """Test that find_and_load successfully raises an error when given an invalid .env"""
-    os.environ.pop("ENVIRONMENT", None)
-    os.environ.pop("EMAIL_USERNAME", None)
-    os.environ.pop("EMAIL_PASSWORD", None)
-    os.environ.pop("EMAIL_HOST", None)
-    os.environ.pop("EMAIL_PORT", None)
-    os.environ.pop("GOOGLE_APPLICATION_CREDENTIALS", None)
     with pytest.raises(OSError):
         find_and_load(tmp_path / "invalid_env_file.env")
 
 
 def test_find_and_load_valid(valid_env_file):
     """Test that find_and_load successfully creates environment variables when given a valid .env"""
-    os.environ.pop("ENVIRONMENT", None)
-    os.environ.pop("EMAIL_USERNAME", None)
-    os.environ.pop("EMAIL_PASSWORD", None)
-    os.environ.pop("EMAIL_HOST", None)
-    os.environ.pop("EMAIL_PORT", None)
-    os.environ.pop("GOOGLE_APPLICATION_CREDENTIALS", None)
-    assert not os.environ.get("ENVIRONMENT")
+    os.environ.pop("MADE_UP_ENV_VAR", None)
+    assert not os.environ.get("MADE_UP_ENV_VAR")
     find_and_load(valid_env_file)
-    assert os.environ["ENVIRONMENT"] == "DEV"
+    assert os.environ["MADE_UP_ENV_VAR"] == "DEV"
 
 
 def test_load_env_vars_invalid(tmp_path):
     """Test that load_env_vars successfully raises an error when given an invalid .env"""
-    os.environ.pop("ENVIRONMENT", None)
-    os.environ.pop("EMAIL_USERNAME", None)
-    os.environ.pop("EMAIL_PASSWORD", None)
-    os.environ.pop("EMAIL_HOST", None)
-    os.environ.pop("EMAIL_PORT", None)
-    os.environ.pop("GOOGLE_APPLICATION_CREDENTIALS", None)
     with pytest.raises(ENVFileDoesnotExist):
         load_env_vars(tmp_path / "invalid_env_file.env")
 
 
 def test_load_env_vars_valid(valid_env_file):
     """Test that load_env_vars successfully creates environment variables when given a valid .env"""
-    os.environ.pop("ENVIRONMENT", None)
-    os.environ.pop("EMAIL_USERNAME", None)
-    os.environ.pop("EMAIL_PASSWORD", None)
-    os.environ.pop("EMAIL_HOST", None)
-    os.environ.pop("EMAIL_PORT", None)
-    os.environ.pop("GOOGLE_APPLICATION_CREDENTIALS", None)
-    assert not os.environ.get("ENVIRONMENT")
+    os.environ.pop("MADE_UP_ENV_VAR", None)
+    assert not os.environ.get("MADE_UP_ENV_VAR")
     load_env_vars(valid_env_file)
-    pprint.pprint(os.environ["ENVIRONMENT"])
-    assert os.environ["ENVIRONMENT"] == "DEV"
+    assert os.environ["MADE_UP_ENV_VAR"] == "DEV"

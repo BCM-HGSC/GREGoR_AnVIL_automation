c331001d10dcd2f81ec33a7d91a4259d005693e8
Ryan Tunkel <123666075+ryanetunkel@users.noreply.github.com>
2024-02-02 14:23:54 -0600

    new integration based testing

--- tests/utils/test_email.py
+++ tests/utils/test_email.py
@@ -1,67 +1,24 @@
 import pytest
-import addict
-from pathlib import Path
+import tempfile
 
-from gregor_anvil_automation.utils.email import send_email, Email, SMTPCredentials
-
-
-@pytest.fixture(name="valid_config")
-def fixture_valid_config():
-    config = addict.Dict(
-        sender="u253196@bcm.edu",
-        recipients="u253196@bcm.edu",
-    )
-    return config
+from gregor_anvil_automation.utils.email import send_email
 
 
-@pytest.fixture(name="invalid_config")
-def fixture_invalid_config():
-    config = addict.Dict(
-        sender="",
-        recipients="",
-    )
-    return config
-
-
-def test_send_email_valid(valid_config):
+@pytest.mark.integration
+def test_send_email_valid_no_attachment(config):
     """Test that send_email successfully sends an email"""
-    subject = "Test Email"
-    body = "This is a test email."
-    attachments = []
-    credentials = SMTPCredentials()
-    email = Email(credentials)
-    email.build_message(subject, body, valid_config.sender, valid_config.recipients)
-    assert send_email(valid_config, subject, body, attachments) == email.send()
+    subject = "Gregor Test Email"
+    body = "This is a Gregor automation test email."
+    result = send_email(config, subject, body, [])
+    assert result == 0
 
 
+@pytest.mark.integration
 def test_send_email_valid_attachment(valid_config):
     """Test that send_email successfully sends an email with an attachment"""
-    subject = "Test Email"
-    body = "This is a test email."
-    attachments = [Path("tests/utils/test_files/email_control.csv")]
-    credentials = SMTPCredentials()
-    email = Email(credentials)
-    email.build_message(subject, body, valid_config.sender, valid_config.recipients)
-    assert send_email(valid_config, subject, body, attachments) == email.send()
-
-
-def test_send_email_valid_empty(valid_config):
-    """Test that send_email successfully sends a blank email"""
-    subject = ""
-    body = ""
-    attachments = []
-    credentials = SMTPCredentials()
-    email = Email(credentials)
-    email.build_message(subject, body, valid_config.sender, valid_config.recipients)
-    assert send_email(valid_config, subject, body, attachments) == email.send()
-
-
-def test_send_email_invalid(invalid_config):
-    """Test that send_email fails to send an email"""
-    subject = "Test Email"
-    body = "This is a test email."
-    attachments = []
-    credentials = SMTPCredentials()
-    email = Email(credentials)
-    email.build_message(subject, body, invalid_config.sender, invalid_config.recipients)
-    assert send_email(invalid_config, subject, body, attachments) == email.send()
+    subject = "Gregor Test Email"
+    body = "This is a Gregor automation test email."
+    test_file = tempfile.TemporaryFile()
+    test_file.writelines("This is a Gregor test file.")
+    result = send_email(valid_config, subject, body, test_file)
+    assert result == 0

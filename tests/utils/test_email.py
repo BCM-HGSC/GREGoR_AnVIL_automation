2c3da6be7f501d8b4d37a92136bb04864f0b38bf
Ryan Tunkel <123666075+ryanetunkel@users.noreply.github.com>
2024-02-02 09:20:54 -0600

    more potential tests

--- tests/utils/test_email.py
+++ tests/utils/test_email.py
@@ -1,7 +1,6 @@
-import os
 import pytest
 import addict
-import pprint
+from pathlib import Path
 
 from gregor_anvil_automation.utils.email import send_email, Email, SMTPCredentials
 
@@ -9,7 +8,17 @@ from gregor_anvil_automation.utils.email import send_email, Email, SMTPCredentia
 @pytest.fixture(name="valid_config")
 def fixture_valid_config():
     config = addict.Dict(
-        ("sender", "u253196@bcm.edu"), ("recipients", "u253196@bcm.edu")
+        sender="u253196@bcm.edu",
+        recipients="u253196@bcm.edu",
+    )
+    return config
+
+
+@pytest.fixture(name="invalid_config")
+def fixture_invalid_config():
+    config = addict.Dict(
+        sender="",
+        recipients="",
     )
     return config
 
@@ -25,6 +34,17 @@ def test_send_email_valid(valid_config):
     assert send_email(valid_config, subject, body, attachments) == email.send()
 
 
+def test_send_email_valid_attachment(valid_config):
+    """Test that send_email successfully sends an email with an attachment"""
+    subject = "Test Email"
+    body = "This is a test email."
+    attachments = [Path("tests/utils/test_files/email_control.csv")]
+    credentials = SMTPCredentials()
+    email = Email(credentials)
+    email.build_message(subject, body, valid_config.sender, valid_config.recipients)
+    assert send_email(valid_config, subject, body, attachments) == email.send()
+
+
 def test_send_email_valid_empty(valid_config):
     """Test that send_email successfully sends a blank email"""
     subject = ""
@@ -34,3 +54,14 @@ def test_send_email_valid_empty(valid_config):
     email = Email(credentials)
     email.build_message(subject, body, valid_config.sender, valid_config.recipients)
     assert send_email(valid_config, subject, body, attachments) == email.send()
+
+
+def test_send_email_invalid(invalid_config):
+    """Test that send_email fails to send an email"""
+    subject = "Test Email"
+    body = "This is a test email."
+    attachments = []
+    credentials = SMTPCredentials()
+    email = Email(credentials)
+    email.build_message(subject, body, invalid_config.sender, invalid_config.recipients)
+    assert send_email(invalid_config, subject, body, attachments) == email.send()

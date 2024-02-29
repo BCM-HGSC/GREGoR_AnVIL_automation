db514126affd11cf844fb35881e82a84cb192d82
ryanetunkel <ryan.tunkel@bcm.edu>
2024-02-29 14:41:33 -0600

    updated genetic_findings field dependencies and added test

--- gregor_anvil_automation/validation/sample.py
+++ gregor_anvil_automation/validation/sample.py
@@ -279,6 +279,17 @@ class SampleValidator(Validator):
         if not value.startswith(tuple(ontology)):
             self._error(field, "Value must start with HP: or MONDO:")
 
+    def _check_with_known_condition_name(self, field: str, value: str):
+        """Checks that field's value is:
+        - NA if gene_known_for_phenotype is Candidate or some string
+        - A string other than NA if gene_known_for_phenotype is Known
+        """
+        gene_known_for_phenotype = self.document.get("gene_known_for_phenotype")
+        if gene_known_for_phenotype == "Known" and value == "NA":
+            self._error(
+                field, "Value may only be NA if gene_known_for_phenotype is not Known"
+            )
+
     def _normalize_coerce_initialcase(self, value: str) -> str:
         """Coerces value to initialcase"""
         if value.strip():

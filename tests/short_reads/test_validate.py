c1ff07ebc9ad5c1fc88d724aa949368531fb120a
ryanetunkel <ryan.tunkel@bcm.edu>
2024-03-18 11:15:01 -0500

    fill out metadata tests

--- tests/short_reads/test_validate.py
+++ tests/short_reads/test_validate.py
@@ -3,8 +3,8 @@ import pytest
 from gregor_anvil_automation.short_reads.validate import apply_metadata_map_file
 
 
-@pytest.fixture(name="csv_dummy_data", scope="function")
-def fixture_csv_dummy_data():
+@pytest.fixture(name="metadata_map_file", scope="function")
+def fixture_metadata_map_file():
     return [
         {
             "experiment_dna_short_read_id": "BCM_TEST-001",
@@ -38,6 +38,37 @@ def fixture_tables():
     }
 
 
-def test_apply_metadata_map_file_pass():
+@pytest.fixture(name="gcp_bucket", scope="function")
+def fixture_gcp_bucket():
+    return "test-gcp-bucket"
+
+
+def test_apply_metadata_map_file_pass(metadata_map_file, tables, gcp_bucket):
     """Tests that the function apply_metadata_map_file passes properly"""
-    return 0
+    experiment_dna_short_read = tables.get("experiment_dna_short_read")
+    aligned_dna_short_read = tables.get("aligned_dna_short_read")
+
+    assert experiment_dna_short_read["experiment_sample_id"] == ""
+    assert aligned_dna_short_read["aligned_dna_short_read_file"] == ""
+    assert aligned_dna_short_read["aligned_dna_short_read_index_file"] == ""
+    assert aligned_dna_short_read["md5sum"] == ""
+
+    apply_metadata_map_file(metadata_map_file, tables, gcp_bucket)
+
+    assert experiment_dna_short_read["experiment_sample_id"] == "BH-#####"
+
+    cram_file_name = "some_name.hgv.cram"
+    aligned_dna_short_read_file_path = f"gs://{gcp_bucket}/{cram_file_name}"
+    assert (
+        aligned_dna_short_read["aligned_dna_short_read_file"]
+        == aligned_dna_short_read_file_path
+    )
+
+    crai_file_name = "some_name.hgv.cram.crai"
+    aligned_dna_short_read_index_file_path = f"gs://{gcp_bucket}/{crai_file_name}"
+    assert (
+        aligned_dna_short_read["aligned_dna_short_read_index_file"]
+        == aligned_dna_short_read_index_file_path
+    )
+
+    assert aligned_dna_short_read["md5sum"] == "FAKESUM-001"

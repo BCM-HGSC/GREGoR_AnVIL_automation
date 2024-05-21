c613578ed651c02afc5c1c02670446761a297d6b
ryanetunkel <ryan.tunkel@bcm.edu>
2024-05-21 12:02:20 -0500

    Add new specific check_with function

--- gregor_anvil_automation/validation/sample.py
+++ gregor_anvil_automation/validation/sample.py
@@ -362,6 +362,22 @@ class SampleValidator(Validator):
                 field, "Value may only exist if variant_type is SNV/INDEL or RE"
             )
 
+    def _check_with_variant_type_is_snv_indel_or_re_and_is_int(self, field: str, value: str):
+        """Checks that field's value is:
+        - An allowed value if variant_type is SNV/INDEL or RE and value is an integer
+        - An empty string otherwise
+        """
+        variant_type = self.document.get("variant_type")
+        if variant_type != "SNV/INDEL" and variant_type != "RE":
+            if value != "":
+                self._error(
+                    field, "Value may only exist if variant_type is SNV/INDEL or RE"
+                )
+        elif not value.isdigit():
+            self._error(
+                field, "Value requires an int"
+            )
+
     def _check_with_variant_type_is_snv_indel_or_re_with_additional_rules(self, field: str, value: str):
         # TODO: Clarify meaning of specifications
         """Checks that field's value is:
